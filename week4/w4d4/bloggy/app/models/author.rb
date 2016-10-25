428e1e9973e24c59899bdd615cb8754e05267f10
dummied <dummied@gmail.com>
2016-10-25 12:15:14 -0400

    Week 5 stuff, includes changes to bloggy

--- week4/w4d4/bloggy/app/models/author.rb
+++ week4/w4d4/bloggy/app/models/author.rb
@@ -1,25 +1,5 @@
-class Author
+class Author < ApplicationRecord
 
-  attr_accessor :name, :bio, :id
-
-  def initialize(id:)
-    self.name = Faker::StarWars.character
-    self.bio = Faker::Hipster.paragraph
-    self.id = id
-  end
-
-  def self.all
-    @authors ||= 3.times.collect do |index|
-      Author.new(id: index + 4)
-    end
-  end
-
-  def self.find(author_id)
-    all.detect { |author| author.id == author_id.to_i }
-  end
-
-  def posts
-    Post.all.select { |post| post.author == self }
-  end
+  has_many :posts
 
 end
--- week4/w4d4/bloggy/config/routes.rb
+++ week4/w4d4/bloggy/config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
   get '/authors/:id' => 'authors#show', as: :author
 
+  get '/search' => 'search#index', as: :search
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
