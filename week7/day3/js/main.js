1f8c6b521a2b63aa066a8e899e038e88eeb85bd4
dummied <dummied@gmail.com>
2016-11-09 12:45:02 -0500

    Adds comments

--- week7/day3/js/main.js
+++ week7/day3/js/main.js
@@ -1,3 +1,7 @@
+// When the document is ready, set the initial state
+// We load the photos from the api
+// If logged in, we show your user info and the photo form
+// If logged out, we hide the photo form and show the sign up and log in forms
 $(document).ready(function(){
   function prepare_doc(){
     load_photos()
@@ -13,22 +17,28 @@ $(document).ready(function(){
 
   prepare_doc()
 
+  // Check if the window has a hash with a number.
+  // If so, pop a modal.
   if(window.location.hash.match(/#\d+/).length > 0) {
+    // This gets just the number part of said hash
     id = window.location.hash.substring(1)
     $('#photo_holder').modal('show')
   }
 
 })
 
+// Clear any alerts on the page by emptying out the #alerts div
 function clear_alerts(){
   $('#alerts').html('')
 }
 
+// Resets the form with an id of the given selector
 function clear_form(selector){
   $(selector)[0].reset();
   toggle_submit(selector)
 }
 
+// Switches a submit button (in the given selector) to a loading image or vice-versa
 function toggle_submit(selector){
   if($(selector + ' input[type=submit]').length > 0){
     $(selector + ' input[type=submit]').replaceWith('<img src="img/ajax-loader.gif" class="loader" />')
@@ -37,6 +47,7 @@ function toggle_submit(selector){
   }
 }
 
+// Handle a successful submission of the login or signup forms (based on the given selector)
 function handle_user_forms(response, selector){
   store_info(response.user)
   clear_alerts()
@@ -46,16 +57,19 @@ function handle_user_forms(response, selector){
   $('#photo_form').show()
 }
 
+// Output the user's information
 function user_info(){
   $('#user_info').html("<h1>Hello, " + username() + "</h1>")
   $('#user_info').append('<a href="#" id="logout" class="btn btn-danger">Logout</a>')
 }
 
+// Store the user credentials in localStorage
 function store_info(response){
   localStorage.setItem('api_token', response.api_token)
   localStorage.setItem('username', response.email)
 }
 
+// Returns the api_token in localStorage or false if it's an empty string
 function api_token(){
   token = localStorage.getItem('api_token')
   if(token === ''){
@@ -65,6 +79,7 @@ function api_token(){
   }
 }
 
+// Returns the username in localStorage or false if it's an empty string
 function username(){
   email = localStorage.getItem('username')
   if(email === ''){
@@ -74,6 +89,8 @@ function username(){
   }
 }
 
+// Logs out a user by blanking the api_token and username
+// And shows the signup/login forms while hiding the user_info and photo upload forms
 function logout(){
   localStorage.setItem('api_token', '')
   localStorage.setItem('username', '')
@@ -82,6 +99,7 @@ function logout(){
   $('#photo_form').hide()
 }
 
+// Helper function to check if we have a logged in user
 function logged_in(){
   if(api_token() != false){
     return true
@@ -90,6 +108,9 @@ function logged_in(){
   }
 }
 
+// Used if a form submission returns an error
+// Populates alerts with what is returned from the server
+// And resets the form
 function handle_errors(selector, response){
   response.responseJSON.forEach(function(err){
     $('#alerts').html('')
@@ -98,12 +119,17 @@ function handle_errors(selector, response){
   toggle_submit(selector)
 }
 
+// Event Handlers
+
+// Click event listener for the links at the top of the page
 $('.click_me').on('click', function(ev){
   ev.preventDefault()
   // console.log(ev)
   alert(ev.target.innerText)
 })
 
+// These two replicate (essentially) a css :hover pseudo-selector
+// By listening for mouseover and mouseout events
 $('.click_me').on('mouseover', function(ev){
   $(ev.target).css("color", "red")
 })
@@ -112,21 +138,26 @@ $('.click_me').on('mouseout', function(ev){
   $(ev.target).css("color", "inherit")
 })
 
+// Hijacks the sign up form and submits it via AJAX to our API instead
 $('#formie').on('submit', function(ev){
-  ev.preventDefault()
+  ev.preventDefault() // Stops the form from doing what it would normally do - namely, submit
   // console.log(Object.keys($(ev.target)[0]))
+  // Remove the submit button so they can't double-submit
   toggle_submit('#formie')
   // console.log($( this ).serializeArray())
+  // Send a POST request to this endpoint
   $.post(
     'https://desolate-sands-90495.herokuapp.com/users',
-    $( this ).serializeArray()
-  ).done(function(response){
+    $( this ).serializeArray() // Turns the form into a string of params that are sent to the server
+  ).done(function(response){ // If the above is successful
     handle_user_forms(response, '#formie')
-  }).fail(function(response){
+  }).fail(function(response){ // If not
     handle_errors('#formie', response)
   })
 })
 
+// Handles the log in form.
+// For most details, see previous
 $('#login').on('submit', function(ev){
   ev.preventDefault()
   // console.log(Object.keys($(ev.target)[0]))
@@ -142,6 +173,8 @@ $('#login').on('submit', function(ev){
   })
 })
 
+// Event listener for our possibly-dynamically-created logout button
+// Hence, the different syntax
 $(document).on('click', '#logout', function(ev){
   ev.preventDefault()
   logout()
@@ -149,6 +182,7 @@ $(document).on('click', '#logout', function(ev){
 
 // Photo upload shtuff
 
+// Load the photos from our API, populate them into #photos
 function load_photos(){
   $.getJSON('https://desolate-sands-90495.herokuapp.com/photos')
   .then(function(response){
@@ -158,25 +192,27 @@ function load_photos(){
   })
 }
 
+// If the photo successfully uploaded, add it to the top of the photo list and clear the form
 function handle_photo_submit(response) {
   $('#photos').prepend('<img src="https://desolate-sands-90495.herokuapp.com/' + response.photo.photo + '" width=200 /><p>' + response.photo.caption + '</p>')
   clear_form('#upload')
 }
 
+// Handles the submission of the photo upload form
 $('#upload').on('submit', function(ev){
   ev.preventDefault()
   toggle_submit('#upload')
   // data = $( this ).serializeArray()
   // data.push({name: 'api_token', value: api_token()})
-  var formData = new FormData(document.getElementById('upload'));
-  formData.append('api_token', api_token())
+  var formData = new FormData(document.getElementById('upload')) // Turn the form into a FormData object
+  formData.append('api_token', api_token()) // Add the api_token to said formdata
   $.post(
     {
       url: 'https://desolate-sands-90495.herokuapp.com/photos',
-      data: formData,
-      processData: false,
-      dataType: 'json',
-      contentType: false
+      data: formData, // This formData object is the data we send to the server
+      processData: false, // We need to tell jQuery not to process this data (and turn it into a string)
+      dataType: 'json', // Set the data type so our server recognizes it
+      contentType: false // Don't set a content type, also so our server recognizes it
     }
   ).done(function(response){
     handle_photo_submit(response)
