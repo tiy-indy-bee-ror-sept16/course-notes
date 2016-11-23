7aeb865798db926e32d86c5b815eb106169b236b
dummied <dummied@gmail.com>
2016-11-23 09:10:51 -0500

    Tuesday stuff

---
+++ week9/day2/men.rb
@@ -0,0 +1,6 @@
+require 'ostruct'
+
+class Men < OpenStruct
+
+
+end
--- week9/day2/parser.rb
+++ week9/day2/parser.rb
@@ -2,14 +2,58 @@ require 'csv'
 
 class Parser
 
-  attr_accessor :file, :klass
-
-  def initialize(file:, klass: Men, other: nil)
-
+  def self.secrets
+    @secrets ||= []
   end
 
-  def other_initialize(file, other=nil, klass=Men)
+  def self.<<(secretive)
+    secrets << secretive
+  end
 
+  attr_accessor :file, :klass
+
+  # def file
+  #   @file
+  # end
+  #
+  # def file=(new_file)
+  #   @file = new_file
+  # end
+
+  def initialize(file:, klass: Men)
+    @file = file
+    self.klass = klass
+    # parse
+  end
+
+  # def klass=(other)
+  #   @klass = other
+  #   notify_the_authorities
+  # end
+
+  def parse
+    self.class.parse(self)
+  end
+
+  def tell_secrets
+    secrets
+  end
+
+  def self.parse(instance)
+    CSV.foreach(instance.file, headers: true, header_converters: :symbol)
+      .collect{ |row| instance.klass.new(row.to_h) }
+  end
+
+
+  # def other_initialize(file, other=nil, klass=Men)
+  #
+  # end
+
+  # protected
+  private
+
+  def secrets
+    "You shouldn't ever see this. It's private"
   end
 
 end
@@ -26,3 +70,9 @@ end
 # end
 #
 # other_initialize("something", Women)
+#
+# local
+# @instance
+# # @@class
+# # $global
+# CONSTANTS
